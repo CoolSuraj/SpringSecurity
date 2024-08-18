99e32d5965bde166f5e685182b27a9513538280b
srjOfficial <suraj2office@duck.com>
2024-08-19 00:42:06 +0530

    Creating In Memory Multiple users and assigning roles to them

--- spring-security/src/main/java/com/suraj/security/spring/spring_security/config/SecurityConfig.java
+++ spring-security/src/main/java/com/suraj/security/spring/spring_security/config/SecurityConfig.java
@@ -4,6 +4,11 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 import static org.springframework.security.config.Customizer.withDefaults;
@@ -21,18 +26,40 @@ public class SecurityConfig {
 	password = spring.security.user.password = 
 		user = spring.security.user.name , if set in application.properties
 	 */
+	
 	@Bean
 	public SecurityFilterChain defaultSecurityFilterChain(HttpSecurity http) throws Exception {
 
 		http.authorizeHttpRequests((requests) -> requests.anyRequest().authenticated());
 		// http.formLogin(withDefaults()); //we can stop this in manual configuration
-		/*
+		/* httpBasic
 		 * In PostMan go to authorization and then select Basic Authorization and give username and password to access 
 		 * this if done from browser it will stay log in until you close the session 
+		 * this makes authorization via header encoded in base64 format
 		 */
 		http.httpBasic(withDefaults());
-
+		/*
+		 * To make it stateless use below Code
+		 * which Means Spring Security will never create an HttpSession and it will 
+		 * never use it to obtain the SecurityContext
+	I	 * In Simple Words it will not set any cookie
+		 */
+		http.sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
 		return http.build();
 
 	}
+	
+	@Bean
+	public UserDetailsService userDetailsService() {
+		
+		/**
+		 * with This In Memory user Details Manager we can create multiple users
+		 * {noop} is used to tell Spring to Store this Password as Plain Text 
+		 */
+		UserDetails user1=User.withUsername("user1").password("{noop}Password1").roles("USER").build();
+		UserDetails admin = User.withUsername("admin").password("{noop}admin1").roles("ADMIN").build();;
+		return new InMemoryUserDetailsManager(user1,admin);
+		
+	}
+	
 }
--- spring-security/src/main/java/com/suraj/security/spring/spring_security/controllers/GreetingController.java
+++ spring-security/src/main/java/com/suraj/security/spring/spring_security/controllers/GreetingController.java
@@ -1,5 +1,6 @@
 package com.suraj.security.spring.spring_security.controllers;
 
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -12,5 +13,19 @@ public class GreetingController {
 		
 		return "Hello World";
 	}
+	
+	
+	@GetMapping("/hiuser")
+	public String sayHiuser() {
+		
+		
+		return "Hello User!!";
+	}
 
+	@GetMapping("/hiBoss")
+	public String sayHiToAdmin() {
+		
+		
+		return "Welcome Admin!!";
+	}
 }
